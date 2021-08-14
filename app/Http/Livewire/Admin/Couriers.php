36c37226a9d052431bd380ad8de3a8a789054ecf
samankassou <samankassoufoulla@gmail.com>
2021-08-14 13:02:08 +0100

    Add a search bar in admin couriers list

--- app/Http/Livewire/Admin/Couriers.php
+++ app/Http/Livewire/Admin/Couriers.php
@@ -10,6 +10,8 @@ class Couriers extends Component
 {
     use WithPagination;
 
+    public $search;
+
     protected $listeners = [
         'courierAdded' => '$refresh',
         'courierUpdated' => '$refresh',
@@ -22,11 +24,31 @@ class Couriers extends Component
     }
     public function render()
     {
-        $couriers = Courier::latest()->paginate(10);
+        $couriers = $this->getCouriers();
         $status = ['En cours', 'Traité', 'Rejeté'];
         return view('livewire.admin.couriers', compact('couriers', 'status'))
             ->extends('layouts.admin', ['title' => "Couriers"])
             ->section('main');
+    }
+
+    public function getCouriers()
+    {
+        $query = Courier::query();
+        $search = '%' . $this->search . '%';
+        $query->where('code', 'LIKE', $search);
+        $query->orWhere('sender', 'LIKE', $search);
+        $query->orWhere('object', 'LIKE', $search);
+        $query->orWhere('status', 'LIKE', $search);
+        $query->orWhere('comments', 'LIKE', $search);
+        $query->orWhereDay('date', $this->search);
+        $query->orWhereMonth('date', $this->search);
+        $query->orWhereYear('date', $this->search);
+        $query->orWhereDate('date', $this->search);
+        $query->orWhereHas('recipient', function ($query) use ($search) {
+            $query->where('name', 'LIKE', $search);
+            $query->orWhere('email', 'LIKE', $search);
+        });
+        return $query->latest()->paginate(10);
     }
 
     public function updateCourierStatus($courierId, $value)
--- resources/views/livewire/admin/couriers.blade.php
+++ resources/views/livewire/admin/couriers.blade.php
@@ -1,7 +1,26 @@
 @section('title', __('Couriers'))
 {{-- New Table --}}
 <div class="w-full overflow-hidden rounded-lg shadow-md">
-    <div class="flex justify-end items-center dark:bg-gray-700">
+    <div class="flex justify-between items-center dark:bg-gray-700">
+        {{-- Search bar --}}
+        <div class="py-4 px-3 pb-0 w-1/3">
+            <div class="relative text-left mb-4">
+                <input
+                    class="appearance-none w-full bg-white border-gray-300 hover:border-gray-500 px-3 py-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-yellow-500 focus:border-2 border"
+                    type="text" placeholder="@lang("Search")" autocomplete="off" wire:model="search">
+                @if ($search)
+                <div class="absolute right-0 top-0 mt-3 mr-4 text-purple-lighter">
+                    <a wire:click.prevent="$set('search', '')" href="#" class="text-gray-400 hover:text-yellow-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </a>
+                </div>
+                @endif
+            </div>
+        </div>
+        {{-- create button --}}
         <div class="m-w-12 my-3 mx-2" wire:click="$emit('openModal', 'admin.couriers.create')">
             <button type="button"
                 class="flex justify-center gap-2 items-center w-full px-3 py-1 text-sm font-medium text-white bg-yellow-600 border border-transparent rounded-md hover:bg-yellow-500 focus:outline-none focus:border-yellow-700 focus:ring-yellow active:bg-yellow-700 transition duration-150 ease-in-out">
@@ -93,7 +112,7 @@
                         </td>
                     </tr>
                 @empty
-                    <tr><td colspan="4">@lang("No item")</td></tr>
+                    <tr><td class="text-center text-gray-500 py-3 px-4" colspan="7">@lang("No item")</td></tr>
                 @endforelse
             </tbody>
         </table>
